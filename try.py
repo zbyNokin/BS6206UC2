9daa97e92b360eee568f1f391b82395f86c29d5b
zbyNokin <dav1dzhangby@gmail.com>
2025-01-12 18:07:48 +0800

    Remove unwanted file from repository

--- try.py
+++
@@ -1,9 +0,0 @@
-import re
-
-mutation = 'p.Q183_R184insQ'
-pos_match = re.search(r'(\d+)_R\d+ins', mutation)
-if pos_match:
-    pos = int(pos_match.group(1))
-    print(pos)  # 输出：183
-else:
-    print("Invalid format")
